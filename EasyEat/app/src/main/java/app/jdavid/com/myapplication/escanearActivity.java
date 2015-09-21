a3dafa845e14e1f260594ff49a8bce0f30dc7286
Julián Rincón <jrincon5@eafit.edu.co>
2015-09-21 17:43:36 -0500

    =)

--- EasyEat/app/src/main/java/app/jdavid/com/myapplication/escanearActivity.java
+++ EasyEat/app/src/main/java/app/jdavid/com/myapplication/escanearActivity.java
@@ -1,16 +1,25 @@
 package app.jdavid.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class escanearActivity extends AppCompatActivity {
+public class escanearActivity extends ActionBarActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escanear);
+
+        Button bscan = (Button) findViewById(R.id.boton_scan);
+        bscan.setOnClickListener(this);
     }
 
     @Override
@@ -22,16 +31,61 @@ public class escanearActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_escanear:
+                Intent scan = new Intent(escanearActivity.this,escanearActivity.class);
+                startActivity(scan);
+                return true;
+            case R.id.action_aliados:
+                scan = new Intent(escanearActivity.this,aliadosActivity.class);
+                startActivity(scan);
+                return true;
+            case R.id.action_menu:
+                scan = new Intent(escanearActivity.this,menuActivity.class);
+                startActivity(scan);
+                return true;
+            case R.id.action_pedidos:
+                scan = new Intent(escanearActivity.this,pedidosActivity.class);
+                startActivity(scan);
+                return true;
+            case R.id.action_factura:
+                scan = new Intent(escanearActivity.this,facturaActivity.class);
+                startActivity(scan);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                contents, Toast.LENGTH_SHORT);
+
+                toast1.show();
+            }
+            if(resultCode == RESULT_CANCELED){
+                //handle cancel
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        System.out.println("Scan");
+        try {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+            startActivityForResult(intent, 0);
+        } catch (Exception e) {
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+            startActivity(marketIntent);
+        }
     }
 }
