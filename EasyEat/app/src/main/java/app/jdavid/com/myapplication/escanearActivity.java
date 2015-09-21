dd616d39611aadadf5f34b0e84448da692d2697e
shergio0312 <shergio0312@gmail.com>
2015-09-21 17:12:24 -0500

    coronamos

--- EasyEat/app/src/main/java/app/jdavid/com/myapplication/escanearActivity.java
+++ EasyEat/app/src/main/java/app/jdavid/com/myapplication/escanearActivity.java
@@ -1,17 +1,30 @@
 package app.jdavid.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class escanearActivity extends AppCompatActivity {
+public class escanearActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private Button scanBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escanear);
+
+        Button bscan = (Button) findViewById(R.id.boton_scan);
+        bscan.setOnClickListener(this);
     }
+
+    //@Override
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +47,41 @@ public class escanearActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                contents, Toast.LENGTH_SHORT);
+
+                toast1.show();
+            }
+            if(resultCode == RESULT_CANCELED){
+                //handle cancel
+            }
+        }
+    }
+
+    public void onClick(View v) {
+        System.out.println("Scan");
+        try {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+            startActivityForResult(intent, 0);
+
+
+        } catch (Exception e) {
+
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+            startActivity(marketIntent);
+
+        }
+
+    }
+
 }
